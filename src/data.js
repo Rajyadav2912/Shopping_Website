6d77c1a375d0ef92ac01eaf5ddcd3f69182fb9e9
Raj Yadav <rajyadav7047@gmail.com>
2024-03-13 12:51:31 +0530

    add data

---
+++ src/data.js
@@ -0,0 +1,239 @@
+export const data = [
+  {
+    product: {
+      men: [
+        {
+          id: "m1",
+          brand: "addidas",
+          title: "Men's Fashion T-Shirt",
+          rating: "5.0",
+          price: "75.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/m1.jpg",
+        },
+        {
+          id: "m2",
+          brand: "addidas",
+          title: "Men's Fashion T-Shirt",
+          rating: "4.8",
+          price: "80.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/m2.jpg",
+        },
+        {
+          id: "m3",
+          brand: "addidas",
+          title: "Men's Fashion T-Shirt",
+          rating: "4.2",
+          price: "85.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/m3.jpg",
+        },
+        {
+          id: "m4",
+          brand: "addidas",
+          title: "Men's Fashion T-Shirt",
+          rating: "4.6",
+          price: "90.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/m4.jpg",
+        },
+        {
+          id: "m5",
+          brand: "addidas",
+          title: "Men's Fashion T-Shirt",
+          rating: "4.5",
+          price: "95.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/m5.jpg",
+        },
+        {
+          id: "m6",
+          brand: "addidas",
+          title: "Men's Fashion T-Shirt",
+          rating: "4.0",
+          price: "50.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/m6.jpg",
+        },
+        {
+          id: "m7",
+          brand: "addidas",
+          title: "Men's Fashion T-Shirt",
+          rating: "3.4",
+          price: "55.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/m7.jpg",
+        },
+        {
+          id: "m8",
+          brand: "addidas",
+          title: "Men's Fashion T-Shirt",
+          rating: "4.5",
+          price: "60.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/m8.jpg",
+        },
+        {
+          id: "m9",
+          brand: "addidas",
+          title: "Men's Fashion T-Shirt",
+          rating: "4.6",
+          price: "65.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/m9.jpg",
+        },
+        {
+          id: "m10",
+          brand: "addidas",
+          title: "Men's Fashion T-Shirt",
+          rating: "4.0",
+          price: "70.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/m10.jpg",
+        },
+      ],
+      women: [
+        {
+          id: "w1",
+          brand: "addidas",
+          title: "Women's Fashion T-Shirt",
+          rating: "5.0",
+          price: "78.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/f1.jpg",
+        },
+        {
+          id: "w2",
+          brand: "addidas",
+          title: "Women's Fashion T-Shirt",
+          rating: "5.0",
+          price: "78.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/f2.jpg",
+        },
+        {
+          id: "w3",
+          brand: "addidas",
+          title: "Women's Fashion T-Shirt",
+          rating: "5.0",
+          price: "78.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/f3.jpg",
+        },
+        {
+          id: "w4",
+          brand: "addidas",
+          title: "Women's Fashion T-Shirt",
+          rating: "5.0",
+          price: "78.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/f4.jpg",
+        },
+        {
+          id: "w5",
+          brand: "addidas",
+          title: "Women's Fashion T-Shirt",
+          rating: "5.0",
+          price: "78.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/f5.jpg",
+        },
+        {
+          id: "w6",
+          brand: "addidas",
+          title: "Women's Fashion T-Shirt",
+          rating: "5.0",
+          price: "78.00",
+          description:
+            "The Gildan Ultra Cotton T-shirt is made form a substantial 6.0 oz. per Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque facilis architecto quis corrupti doloribus, nihil unde necessitatibus neque perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolorem maiores accusantium doloremque animi necessitatibus quidem alias, sit nam velit!",
+          image: "./assets/products/f6.jpg",
+        },
+      ],
+    },
+    feature: [
+      {
+        id: 1,
+        title: "Free Shipping",
+        image: "./assest/features/f1.jpg",
+      },
+      {
+        id: 2,
+        title: "Online Order",
+        image: "./assest/features/f2.jpg",
+      },
+      {
+        id: 3,
+        title: "Save Money",
+        image: "./assest/features/f3.jpg",
+      },
+      {
+        id: 4,
+        title: "Promotions",
+        image: "./assest/features/f4.jpg",
+      },
+      {
+        id: 5,
+        title: "Happy Sell",
+        image: "./assest/features/f5.jpg",
+      },
+      {
+        id: 6,
+        title: "Full 24x7 Support",
+        image: "./assest/features/f6.jpg",
+      },
+    ],
+    blog: [
+      {
+        id: "b1",
+        title: "The Cotton-Jersey Zip-Up Hoodies",
+        text: "Kickstarter man braid godard coloring book. Raclette waistcoat selfies yr wolf chartreus hexagon irony, godard. Kickstarter man braid godard coloring book. Raclette waistcoat selfies yr wolf chartreus hexagon irony, godard...",
+        image: "./assest/blog/b1.jpg",
+        date: "13/01",
+      },
+      {
+        id: "b2",
+        title: "How to Style a Quiff",
+        text: "Kickstarter man braid godard coloring book. Raclette waistcoat selfies yr wolf chartreus hexagon irony, godard. Kickstarter man braid godard coloring book. Raclette waistcoat selfies yr wolf chartreus hexagon irony, godard...",
+        image: "./assest/blog/b2.jpg",
+        date: "13/01",
+      },
+      {
+        id: "b3",
+        title: "Must-Have Skater Girl Items",
+        text: "Kickstarter man braid godard coloring book. Raclette waistcoat selfies yr wolf chartreus hexagon irony, godard. Kickstarter man braid godard coloring book. Raclette waistcoat selfies yr wolf chartreus hexagon irony, godard...",
+        image: "./assest/blog/b3.jpg",
+        date: "13/01",
+      },
+      {
+        id: "b4",
+        title: "Runway-Inspired Trends",
+        text: "Kickstarter man braid godard coloring book. Raclette waistcoat selfies yr wolf chartreus hexagon irony, godard. Kickstarter man braid godard coloring book. Raclette waistcoat selfies yr wolf chartreus hexagon irony, godard...",
+        image: "./assest/blog/b4.jpg",
+        date: "13/01",
+      },
+      {
+        id: "b5",
+        title: "AW20 Menswear Trends",
+        text: "Kickstarter man braid godard coloring book. Raclette waistcoat selfies yr wolf chartreus hexagon irony, godard. Kickstarter man braid godard coloring book. Raclette waistcoat selfies yr wolf chartreus hexagon irony, godard...",
+        image: "./assest/blog/b5.jpg",
+        date: "13/01",
+      },
+    ],
+  },
+];
